740637785ed5b7d5ae1077bce817f4cd0aa9e285
Jose Muller <joseleps3@gmail.com>
2024-05-22 22:04:38 -0300

    modificaciones de css y js

--- js/validacion.js
+++ js/validacion.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById("formulario"); // Selecciona el formulario por su ID
+    const form = document.getElementById("formulario"); 
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
@@ -12,23 +12,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
         let isValid = true;
 
-        const nameRegex = /^[A-Za-z\s]+$/;
-        if (!nameRegex.test(name.value.trim())) {
+        // Validación del nombre
+        const namePattern = /^[A-Za-z\s]+$/;
+        if (!namePattern.test(name.value.trim())) {
             isValid = false;
             alert("Por favor ingrese un nombre válido.");
         }
 
         // Validación del correo electrónico
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email.value)) {
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailPattern.test(email.value)) {
             isValid = false;
             alert("Por favor ingrese un correo electrónico válido.");
         }
 
         // Validación de la fecha de nacimiento
-        const currentDate = new Date();
-        const birthDate = new Date(cumple.value);
-        if (birthDate >= currentDate) {
+        const fechaActual = new Date();
+        const fechaNacimiento = new Date(cumple.value);
+        if (fechaNacimiento >= fechaActual) {
             isValid = false;
             alert("Por favor ingrese una fecha de nacimiento válida.");
         }
